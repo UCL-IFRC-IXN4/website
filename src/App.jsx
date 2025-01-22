c60b65ea1f472f7e2f564c61c8bf2930be186dbc
Dylan Penney <penneydylan@icloud.com>
2025-01-22 23:13:27 +0000

    pls deploy

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Content from "./components/Content/Content";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Header />
       <Routes>
         <Route
