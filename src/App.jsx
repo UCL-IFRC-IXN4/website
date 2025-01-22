e8fe3f7d0f955622ea3b0328a1291383462f02b3
Dylan Penney <penneydylan@icloud.com>
2025-01-22 23:20:02 +0000

    idk

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import "./App.css";
-import Footer from "./components/Footer/Footer";
 import Content from "./components/Content/Content";
 
 function App() {
